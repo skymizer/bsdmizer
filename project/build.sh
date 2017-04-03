f444687ce1073ec760795f8498ad7b2ebd940923
Li-Wen Hsu <lwhsu@lwhsu.org>
2017-04-03 07:22:21 +0000

    Add simple build script

---
+++ project/build.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+_BINPATH=`realpath $0`
+_DIRNAME=`dirname ${_BINPATH}`
+BASEDIR=`realpath ${_DIRNAME}/..`
+
+SRCDIR=${BASEDIR}/freebsd
+
+export MAKEOBJDIRPREFIX=${BASEDIR}/obj
+MAKECONF=/dev/null
+SRCCONF=/dev/null
+
+JFLAG=$(sysctl -n kern.smp.cpus)
+
+TARGET=amd64
+TARGET_ARCH=amd64
+KERNCONF=GENERIC
+
+cd ${SRCDIR}
+
+make -j ${JFLAG} buildworld \
+        TARGET=${TARGET} \
+        TARGET_ARCH=${TARGET_ARCH} \
+        __MAKE_CONF=${MAKECONF} \
+        SRCCONF=${SRCCONF}
+make -j ${JFLAG} buildkernel \
+        TARGET=${TARGET} \
+        TARGET_ARCH=${TARGET_ARCH} \
+        KERNCONF=${KERNCONF} \
+        __MAKE_CONF=${MAKECONF} \
+        SRCCONF=${SRCCONF}
