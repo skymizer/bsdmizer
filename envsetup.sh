5622b57b8f4149f150f85bdbcf54d1b642986830
Li-Wen Hsu <lwhsu@lwhsu.org>
2017-04-11 21:50:19 +0800

    Build libnetbsd for test

--- envsetup.sh
+++ envsetup.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
 SVN_REVISION=316462
+SRCDIR=freebsd
+
+JFLAG=$(sysctl -n kern.smp.cpus)
 
 svn co svn://svn.freebsd.org/base/head@${SVN_REVISION} freebsd
+
+cd ${SRCDIR}/lib/libnetbsd
+make -j ${JFLAG} all
