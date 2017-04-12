9f5d0b54350d247bda4a8f732eab19236b7d1e2e
Li-Wen Hsu <lwhsu@lwhsu.org>
2017-04-12 22:04:34 +0800

    Return median

--- benchmark/msun-bench.sh
+++ benchmark/msun-bench.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-sudo dtrace -s msun-bench.d -c "./msun-bench $1" $1
+ITERATION=5
+MEDIAN=$(((${ITERATION}+1)/2))
+
+R=""
+for i in `jot 5`; do
+	T=`sudo dtrace -s msun-bench.d -c "./msun-bench $1" $1`
+	R="${R}${T}\n"
+done
+
+printf "${R}" | sort -n | head -n ${MEDIAN} | tail -n 1
