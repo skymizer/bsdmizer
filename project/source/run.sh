f481ab0093d94d14e6980e05d3a9d1ccc7326371
Li-Wen Hsu <lwhsu@lwhsu.org>
2017-04-21 03:36:11 +0800

    Really run benchmark, disable kyua test for now due to path issue

--- project/source/run.sh
+++ project/source/run.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-_BINPATH=`realpath $0`
-_DIRNAME=`dirname ${_BINPATH}`
-BASEDIR=`realpath ${_DIRNAME}/..`
-
 SRCDIR=`realpath ../../../freebsd`
-export MAKEOBJDIRPREFIX=`realpath ./obj`
 
-cd ${SRCDIR}/lib/msun/tests
-env LD_PRELOAD=${SRCDIR}/lib/msun/libm.so.5 kyua test
+OBJDIR="`pwd`/obj"
+export MAKEOBJDIRPREFIX=${OBJDIR}
+
+#cd ${SRCDIR}/lib/msun
+#OBJDIR=`make -V .OBJDIR`
+#cd ${SRCDIR}/lib/msun/tests
+#env LD_PRELOAD=${OBJDIR}/libm.so.5 kyua test
+
+../../../benchmark/msun-bench.sh exp
