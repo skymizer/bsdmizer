112deba08c09e167bdad069f749fcab19a13e042
Li-Wen Hsu <lwhsu@lwhsu.org>
2017-04-20 19:27:16 +0800

    Use amd64-gcc

--- project/build.sh
+++ project/build.sh
@@ -9,4 +9,10 @@ SRCDIR=${BASEDIR}/freebsd
 JFLAG=$(sysctl -n kern.smp.cpus)
 
 cd ${SRCDIR}/lib/msun
-make -j ${JFLAG} all
+make -j ${JFLAG} \
+	CC=/usr/local/bin/x86_64-unknown-freebsd11.0-gcc \
+	CXX=/usr/local/bin/x86_64-unknown-freebsd11.0-g++ \
+	CPP=/usr/local/bin/x86_64-unknown-freebsd11.0-cpp \
+	CROSS_BINUTILS_PREFIX=/usr/local/x86_64-freebsd/bin/ \
+	COMPILER_TYPE=gcc \
+	all
