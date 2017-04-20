3a822d343efc491d8ff10448a634119c37772f47
Li-Wen Hsu <lwhsu@lwhsu.org>
2017-04-21 03:35:08 +0800

    Cleaning is easy now, just remove obj dir name make.conf

--- project/source/clean.sh
+++ project/source/clean.sh
@@ -1,13 +1,3 @@
 #!/bin/sh
 
-_BINPATH=`realpath $0`
-_DIRNAME=`dirname ${_BINPATH}`
-BASEDIR=`realpath ${_DIRNAME}/..`
-
-SRCDIR=`realpath ../../../freebsd`
-
-JFLAG=$(sysctl -n kern.smp.cpus)
-
-export MAKEOBJDIRPREFIX=`realpath ./obj`
-cd ${SRCDIR}/lib/msun
-make cleandir
+rm -fr obj make.conf
