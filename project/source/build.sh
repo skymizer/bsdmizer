3e86db604e202cbea4ade5a0c9f04c239003337f
Li-Wen Hsu <lwhsu@lwhsu.org>
2017-04-21 02:57:54 +0800

    Overhaul build script

--- project/source/build.sh
+++ project/source/build.sh
@@ -1,48 +1,36 @@
 #!/bin/sh
 
 OPTIONS=$@
+echo "CFLAGS=${OPTIONS}" > make.conf
+MAKECONF=`realpath make.conf`
+
 SRCDIR=`realpath ../../../freebsd`
 
-JFLAG=$(sysctl -n kern.smp.cpus)
-mkdir obj
-export MAKEOBJDIRPREFIX=`realpath ./obj`
-echo "MAKEOBJDIRPREFIX: ${MAKEOBJDIRPREFIX}"
+#JFLAG=$(sysctl -n kern.smp.cpus)
 
 CROSS_BINUTILS_PREFIX=/usr/local/x86_64-freebsd/bin/
+MAKE="make \
+	-D WITHOUT_MAN \
+	__MAKE_CONF=${MAKECONF} \
+	CC=/usr/local/bin/x86_64-unknown-freebsd11.0-gcc \
+	CXX=/usr/local/bin/x86_64-unknown-freebsd11.0-g++ \
+	CPP=/usr/local/bin/x86_64-unknown-freebsd11.0-cpp \
+	AS=${CROSS_BINUTILS_PREFIX}as \
+	AR=${CROSS_BINUTILS_PREFIX}ar \
+	LD=${CROSS_BINUTILS_PREFIX}ld \
+	NM=${CROSS_BINUTILS_PREFIX}nm \
+	OBJCOPY=${CROSS_BINUTILS_PREFIX}objcopy \
+	RANLIB=${CROSS_BINUTILS_PREFIX}ranlib \
+	SIZE=${CROSS_BINUTILS_PREFIX}size \
+	COMPILER_TYPE=gcc"
+
+OBJDIR="`pwd`/obj"
+export MAKEOBJDIRPREFIX=${OBJDIR}
 
 cd ${SRCDIR}/lib/libnetbsd
-make -j ${JFLAG} \
-	CC=/usr/local/bin/x86_64-unknown-freebsd11.0-gcc \
-	CXX=/usr/local/bin/x86_64-unknown-freebsd11.0-g++ \
-	CPP=/usr/local/bin/x86_64-unknown-freebsd11.0-cpp \
-	AS=${CROSS_BINUTILS_PREFIX}as \
-	AR=${CROSS_BINUTILS_PREFIX}ar \
-	LD=${CROSS_BINUTILS_PREFIX}ld \
-	NM=${CROSS_BINUTILS_PREFIX}nm \
-	OBJCOPY=${CROSS_BINUTILS_PREFIX}objcopy \
-	RANLIB=${CROSS_BINUTILS_PREFIX}ranlib \
-	SIZE=${CROSS_BINUTILS_PREFIX}size \
-	COMPILER_TYPE=gcc \
-	all
+${MAKE} obj
+${MAKE} all
 
 cd ${SRCDIR}/lib/msun
-make -j ${JFLAG} \
-	CC=/usr/local/bin/x86_64-unknown-freebsd11.0-gcc \
-	CXX=/usr/local/bin/x86_64-unknown-freebsd11.0-g++ \
-	CPP=/usr/local/bin/x86_64-unknown-freebsd11.0-cpp \
-	AS=${CROSS_BINUTILS_PREFIX}as \
-	AR=${CROSS_BINUTILS_PREFIX}ar \
-	LD=${CROSS_BINUTILS_PREFIX}ld \
-	NM=${CROSS_BINUTILS_PREFIX}nm \
-	OBJCOPY=${CROSS_BINUTILS_PREFIX}objcopy \
-	RANLIB=${CROSS_BINUTILS_PREFIX}ranlib \
-	SIZE=${CROSS_BINUTILS_PREFIX}size \
-	COMPILER_TYPE=gcc \
-	all
-
-if [ $? -eq 0 ]; then
-  exit 0;
-else
-  echo "build failed!!!!";
-  exit 1;
-fi
+${MAKE} obj
+${MAKE} all
