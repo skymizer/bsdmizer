08eb1cf1fa2b4fc0ba535b119cf3fffcc1c90c6b
Li-Wen Hsu <lwhsu@lwhsu.org>
2017-04-21 03:31:28 +0800

    Use absolute path for all arguments

--- benchmark/msun-bench.sh
+++ benchmark/msun-bench.sh
@@ -5,12 +5,15 @@ if [ $# -lt 1 ]; then
 	exit 1
 fi
 
+_BINPATH=`realpath $0`
+BINDIR=`dirname ${_BINPATH}`
+
 ITERATION=5
 MEDIAN=$(((${ITERATION}+1)/2))
 
 R=""
 for i in `jot ${ITERATION}`; do
-	T=`sudo dtrace -s msun-bench.d -c "./msun-bench $1" $1`
+	T=`sudo dtrace -s ${BINDIR}/msun-bench.d -c "${BINDIR}/msun-bench $1 ${BINDIR}/data" $1`
 	R="${R}${T}\n"
 done
 
