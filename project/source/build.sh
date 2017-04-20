265cd5df84341ec23f8cb439c8d4ed1f7e03b58e
Li-Wen Hsu <lwhsu@lwhsu.org>
2017-04-20 20:06:45 +0800

    Specify binutils path

--- project/source/build.sh
+++ project/source/build.sh
@@ -6,12 +6,20 @@ SRCDIR=`realpath ../../../freebsd`
 JFLAG=$(sysctl -n kern.smp.cpus)
 export MAKEOBJDIRPREFIX=./obj
 
+CROSS_BINUTILS_PREFIX=/usr/local/x86_64-freebsd/bin/
+
 cd ${SRCDIR}/lib/msun
 make -j ${JFLAG} \
 	CC=/usr/local/bin/x86_64-unknown-freebsd11.0-gcc \
 	CXX=/usr/local/bin/x86_64-unknown-freebsd11.0-g++ \
 	CPP=/usr/local/bin/x86_64-unknown-freebsd11.0-cpp \
-	CROSS_BINUTILS_PREFIX=/usr/local/x86_64-freebsd/bin/ \
+	AS=${CROSS_BINUTILS_PREFIX}as \
+	AR=${CROSS_BINUTILS_PREFIX}ar
+	LD=${CROSS_BINUTILS_PREFIX}ld
+	NM=${CROSS_BINUTILS_PREFIX}mn
+	OBJCOPY=${CROSS_BINUTILS_PREFIX}objcopy
+	RANLIB=${CROSS_BINUTILS_PREFIX}ranlib
+	SIZE=${CROSS_BINUTILS_PREFIX}size
 	COMPILER_TYPE=gcc \
 	all
 
