0604fa29fa72a0884ec8dbd3ecff09e8b1ca27f4
Li-Wen Hsu <lwhsu@lwhsu.org>
2017-04-24 00:27:07 +0800

    Specify libm path for benchmarking

--- project/source/run.sh
+++ project/source/run.sh
@@ -4,10 +4,11 @@ SRCDIR=`realpath ../../../freebsd`
 
 OBJDIR="`pwd`/obj"
 export MAKEOBJDIRPREFIX=${OBJDIR}
+cd ${SRCDIR}/lib/msun
+LIBPATH=`make -V .OBJDIR`
+cd -
 
-#cd ${SRCDIR}/lib/msun
-#OBJDIR=`make -V .OBJDIR`
 #cd ${SRCDIR}/lib/msun/tests
 #env LD_PRELOAD=${OBJDIR}/libm.so.5 kyua test
 
-../../../benchmark/msun-bench.sh sin
+../../../benchmark/msun-bench.sh pow ${LIBPATH}/libm.so.5
