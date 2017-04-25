4083a6750880671a5fa15f3bde16f643c0d17d5f
Li-Wen Hsu <lwhsu@lwhsu.org>
2017-04-25 23:46:31 +0800

    Set LD_PRELOAD libbz2

--- project/source/run.sh
+++ project/source/run.sh
@@ -7,13 +7,19 @@ SRCDIR=`realpath ../../../freebsd`
 
 OBJDIR="`pwd`/obj"
 export MAKEOBJDIRPREFIX=${OBJDIR}
+
 cd ${SRCDIR}/usr.bin/bzip2
-CANONICALOBJDIR=`make -V CANONICALOBJDIR`
+BZIP2_BINDIR=`make -V CANONICALOBJDIR`
+cd -
+
+cd ${SRCDIR}/lib/libbz2
+BZIP2_LIBDIR=`make -V CANONICALOBJDIR`
 cd -
 
 R=""
 for i in `jot ${ITERATION}`; do
-	T=`sudo LD_PRELOAD=$2 dtrace -s bzip2-bench.d -c "${CANONICALOBJDIR}/bzip2 -k -9 data"`
+	T=`sudo LD_PRELOAD=${BZIP2_LIBDIR}/libbz2.so.4 dtrace -s bzip2-bench.d -c "${BZIP2_BINDIR}/bzip2 -k -9 data"`
+	#T=`sudo dtrace -s bzip2-bench.d -c "${BZIP2_BINDIR}/bzip2 -k -9 data"`
 	diff data.bz2.orig data.bz2
 	if [ $? -ne 0 ]; then
 		exit 1
